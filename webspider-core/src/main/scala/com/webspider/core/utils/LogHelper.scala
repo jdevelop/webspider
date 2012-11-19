1a0819810c0d993db74227116f1985e56a37d699
xdev <xaoc3dgraph@gmail.com>
2012-11-19 21:35:33 +0200

    Added log4j

--- webspider-core/src/main/scala/com/webspider/core/utils/LogHelper.scala
+++ webspider-core/src/main/scala/com/webspider/core/utils/LogHelper.scala
@@ -1,6 +1,14 @@
 package com.webspider.core.utils
 
+import org.apache.log4j.Logger
+
 trait LogHelper {
-  def debug(msg: Any) = println(msg)
-  def error(msg: Any) = println(msg)
+
+  val loggerName = this.getClass.getName
+  lazy val logger = Logger.getLogger(loggerName)
+
+  def debug(msg: => Any) = if (logger.isDebugEnabled) logger.debug(msg)
+  def debug(msg: => Any, throwable: => Throwable) = if (logger.isDebugEnabled) logger.debug(msg, throwable)
+  def error(msg: => Any) = logger.error(msg)
+  def error(msg: => Any, throwable: => Throwable) = logger.error(msg, throwable)
 }
