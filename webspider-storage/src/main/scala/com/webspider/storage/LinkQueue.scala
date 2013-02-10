f2959f1f8a87824328d48accc8120749a69b1798
Eugene Dzhurinsky <jdevelop@gmail.com>
2013-02-09 22:47:36 -0500

    [ 5 ] Ongoing development - add pop method implementation and traversal of in-progress database.

--- webspider-storage/src/main/scala/com/webspider/storage/LinkQueue.scala
+++ webspider-storage/src/main/scala/com/webspider/storage/LinkQueue.scala
@@ -1,7 +1,7 @@
 package com.webspider.storage
 
 import com.webspider.core.Link
-import com.webspider.storage.LinkQueue.AddResult
+import com.webspider.storage.LinkQueue.{PopError, AddResult}
 import java.util.UUID
 
 object LinkQueue {
@@ -12,16 +12,24 @@ object LinkQueue {
 
   case object Retry extends AddResult
 
+  sealed abstract class PopError
+
+  case object AlreadyInProgress extends PopError
+
+  case object NoRecordInDatabase extends PopError
+
+  case class GenericException(e: Throwable) extends PopError
+
 }
 
 trait LinkQueue {
 
   def push(link: Link, parent: UUID): AddResult
 
-  def pop(): Option[Link]
+  def pop(): Either[PopError, Link]
 
   def reset()
 
-  def queueSize(): Int
+  def queueSize(): Long
 
 }
